a3655fce689c175f13b665f7769c4b14dc50c29e
elinapekka <90378752+elinapekka@users.noreply.github.com>
2023-02-04 15:14:48 +0200

    Adding new method...

--- Desktop/school/2023Spring/ServerProgramming/elinabookstore/src/main/java/com/example/elinabookstore/web/BookController.java
+++ Desktop/school/2023Spring/ServerProgramming/elinabookstore/src/main/java/com/example/elinabookstore/web/BookController.java
@@ -1,13 +1,28 @@
 package com.example.elinabookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class BookController {
+import com.example.elinabookstore.model.BookRepository;
 
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
+	
 	@RequestMapping("/index")
 	public String bookForm(Model model) {
 		return "index";
+	}
+	
+	
+	@RequestMapping(value = {"/", "/booklist"})
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 }
